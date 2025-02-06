421694ef5eb49f49cce458b2dc4cdc6c501a32a3
Ece Eren <aybikeceren@gmail.com>
2025-02-06 23:29:01 +0300

    Initial commit

---
+++ src/main/java/com/example/student_assignment_submission/StudentAssignmentSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_assignment_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAssignmentSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAssignmentSubmissionApplication.class, args);
+	}
+
+}
